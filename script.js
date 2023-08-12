18ea4ea4046e8fe646e177897a5a02b33248a3dd
Akash sharma <114904411+Polo080103@users.noreply.github.com>
2023-08-12 14:25:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const input = document.querySelector("#input");
 const infText = document.querySelector("#info-text");
 const meaningContainer = document.querySelector(".meaning-container");
-const tile = document.querySelector("#title");
+const titleEl = document.querySelector("#title");
 const meaning = document.querySelector("#meaning");
 const example = document.querySelector("#example");
 const voice = document.querySelector("#audio");
@@ -15,17 +15,27 @@ async function fetchAPI(word){
         infText.style.display = "block";
         const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
         const result = await fetch(URL).then((res) => res.json());
-        console.log(result);
-        console.log(result[0].meanings[0].definitions[0].definition);
-        // console.log(title);
-        infText.style.display = "none";
-        meaningContainer.style.display = "block";   
-        tile.innerHTML = result[0].word;     
-        meaning.innerHTML = result[0].meanings[0].definitions[0].definition; 
-        if(result[0].meanings[0].definitions[0].example){
-            example.innerHTML = result[0].meanings[0].definitions[0].example;   
+        if(result.title){
+            meaningContainer.style.display = "block";   
+            titleEl.innerHTML = result.title;     
+            meaning.innerHTML = result.resolution; 
+            infText.style.display = "none";
+            voice.style.display="none";
         }
-        voice.src = result[0].phonetics[0].audio;     
+        else{
+            infText.style.display = "none";
+            meaningContainer.style.display = "block";   
+            titleEl.innerHTML = result[0].word;     
+            meaning.innerHTML = result[0].meanings[0].definitions[0].definition; 
+            if(result[0].meanings[0].definitions[0].example){
+                example.innerHTML = result[0].meanings[0].definitions[0].example;   
+            }
+            voice.src = result[0].phonetics[0].audio; 
+        }
+
+
+
+            
     } 
     catch (error) {
         console.log(error);
